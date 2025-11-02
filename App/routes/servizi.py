0fbfcd98ac6fc40596f72df1835f15b6652100a9
Federico Macchi <macchifederico@libero.it>
2025-11-02 19:17:11 +1100

    servizi.py

--- App/routes/servizi.py
+++ App/routes/servizi.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-servizi_bp = Blueprint('home', __name__)
+servizi_bp = Blueprint('servizi', __name__)
 
 @servizi_bp.route('/')
-def home():
+def servizi():
     return render_template('servizi.html')
